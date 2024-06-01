f207bd34be0341c9e3ad98c8853d88a08c3c197a
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-01 14:19:04 +0200

    authentication

--- base/views.py
+++ base/views.py
@@ -1,13 +1,46 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
 from .models import Room, Subject
 from .forms import RoomForm
+
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                messages.error(request, "incorrect password")
+        except:
+            messages.error(request, "Incorrect username")
+
+        
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
 
-    rooms = Room.objects.filter(subject__name__icontains=q)
+    rooms = Room.objects.filter(Q(subject__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
+    
     subjects = Subject.objects.all()
-    return render(request, 'base/home.html', {"rooms": rooms, "subjects": subjects})
+    room_count = rooms.count()
+
+    context = {"rooms": rooms, "subjects": subjects, "room_count": room_count}
+    return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
